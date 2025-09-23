26331bab28b5025273e2eec8f211fdef1cc97309
Sarathkumar R <167192152+sarathkumarRk@users.noreply.github.com>
2025-09-23 16:52:54 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+go_summary=("ubergo", "chennai", "ariport", 450.00, "completed")
+# print(go_summary)
+
+# print(go_summary[1])
+
+# for item in go_summary:
+#     print(item)
+
+print(go_summary.count("completed"))
+print(go_summary.index("completed"))
